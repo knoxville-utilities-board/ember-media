f2c22de3520349655c2c4be2a035fa61fab8e2ac
Tyler Senter <tyler.senter@kub.org>
2025-12-29 15:54:04 -0500

    fix(ci): Update commit message script (#5)

--- .github/release-commit-message.js
+++ .github/release-commit-message.js
@@ -1,34 +1,17 @@
-import { readFileSync, readdirSync, statSync } from 'fs';
-import { join } from 'path';
+import { readFileSync } from 'fs';
 
 const releasePlan = readFileSync('.release-plan.json', 'utf8');
 const releasePlanJson = JSON.parse(releasePlan);
 
-const packageDirectory = 'packages';
-const packages = readdirSync(packageDirectory)
-  .map((file) => join(packageDirectory, file))
-  .filter((path) => statSync(path).isDirectory());
+let updatedVersion;
+const json = readFileSync('package.json', 'utf8');
+const packageJson = JSON.parse(json);
 
-const updatedVersions = [];
-for (const pkg of packages) {
-  let packageJson;
-  try {
-    const json = readFileSync(join(pkg, 'package.json'), 'utf8');
-    packageJson = JSON.parse(json);
-  } catch {
-    continue;
-  }
-
-  if (packageJson.private) {
-    continue;
-  }
-
-  const name = packageJson.name;
-  const newVersion = releasePlanJson.solution[name]?.newVersion;
-  if (newVersion) {
-    updatedVersions.push(`${name}@${newVersion}`);
-  }
+const name = packageJson.name;
+const newVersion = releasePlanJson.solution[name]?.newVersion;
+if (newVersion) {
+  updatedVersion = `${name}@${newVersion}`;
 }
 
-const commitMessage = `COMMIT_MESSAGE=release: ${updatedVersions.join(', ')}`;
+const commitMessage = `COMMIT_MESSAGE=release: ${updatedVersion}`;
 console.log(commitMessage);
